d9b228cb74e719f053d995469fc9052c07112ee4
Pedro <pedrogarcia.kz@gmail.com>
2025-01-21 20:09:56 -0300

    Projeto Nivel LogicaDeProgramacao-DIO

---
+++ ProjetoNivel.py
@@ -0,0 +1,22 @@
+nome = input("DIgite o nome do seu heroi: ")
+xp = int(input("Digite o numero de exp do seu heroi: "))
+nivel = ""
+
+if xp <= 1000:
+    nivel = "Ferro"
+elif 1001 <= xp <= 2000:
+    nivel = "Bronze"
+elif 2001 <= xp <= 5000:
+    nivel = "Prata"
+elif 5001 <= xp <= 7000:
+    nivel = "Ouro"
+elif 7001 <= xp <= 8000:
+    nivel = "Platina"
+elif 8001 <= xp <= 9000:
+    nivel = "Ascendente"
+elif 9001 <= xp <= 10000:
+    nivel = "Imortal"
+else:
+    nivel = "Radiante"
+
+print(f"O jogador {nome} está no nível {nivel}.")
